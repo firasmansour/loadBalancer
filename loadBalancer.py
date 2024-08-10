7f35179c741a2cc6283cda345d69212828bc435d
firasmansour <103880221+firasmansour@users.noreply.github.com>
2024-08-11 00:02:35 +0300

    Update loadBalancer.py

--- loadBalancer.py
+++ loadBalancer.py
@@ -1,21 +1,24 @@
-import socket
-import SocketServer
-import Queue
-import sys
-import time
-import threading
-
+# uncompyle6 version 3.9.2
+# Python bytecode version base 2.7 (62211)
+# Decompiled from: Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)]
+# Embedded file name: ./exampleLB.py
+# Compiled at: 2015-12-28 12:42:45
+import socket, SocketServer, Queue, sys, time, threading
 HTTP_PORT = 80
 previous_server = 3
 lock = threading.Lock()
 SERV_HOST = '10.0.0.1'
 servers = {'serv1': ('192.168.0.101', None), 'serv2': ('192.168.0.102', None), 'serv3': ('192.168.0.103', None)}
 
-# Define a queue to store incoming requests
-request_queue = Queue.Queue()
-
 def LBPrint(string):
-    print('%s: %s-----' % (time.strftime('%H:%M:%S', time.localtime(time.time())), string))
+    print '%s : %s-----' % (time.strftime('%H:%M:%S', time.localtime(time.time())), string)
+
+
+
+
+
+
+
 
 def createSocket(addr, port):
     for res in socket.getaddrinfo(addr, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
@@ -42,65 +45,68 @@ def createSocket(addr, port):
         sys.exit(1)
     return new_sock
 
+
 def getServerSocket(servID):
     name = 'serv%d' % servID
     return servers[name][1]
+
 
 def getServerAddr(servID):
     name = 'serv%d' % servID
     return servers[name][0]
 
-def getNextServer():
+
+def getNextServer(req_type, req_time):
     global lock
     global previous_server
     lock.acquire()
-    next_server = previous_server % 3 + 1
+    if req_type == "V" or req_type == "P":
+        if previous_server == 1:
+            next_server = 2
+        else if previous_server == 2 :
+            next_server = 1
+    else 
+        next_server = 3
     previous_server = next_server
     lock.release()
     return next_server
 
-def parseRequest(req):
-    return req[0], req[1]
 
-def processQueue():
-    while True:
-        client_sock = request_queue.get()
-        try:
-            req = client_sock.recv(2)
-            req_type, req_time = parseRequest(req)
-            servID = getNextServer()
-            LBPrint('received request %s from %s, sending to %s' % (req, client_sock.getpeername()[0], getServerAddr(servID)))
-            serv_sock = getServerSocket(servID)
-            serv_sock.sendall(req)
-            data = serv_sock.recv(2)
-            client_sock.sendall(data)
-        except Exception as e:
-        finally:
-            client_sock.close()
+def parseRequest(req):
+    return (
+     req[0], req[1])
+
 
 class LoadBalancerRequestHandler(SocketServer.BaseRequestHandler):
+
     def handle(self):
         client_sock = self.request
-        # Add the request to the queue
-        request_queue.put(client_sock)
+        req = client_sock.recv(2)
+        req_type, req_time = parseRequest(req)
+        servID = getNextServer(req_type, req_time)
+        LBPrint('recieved request %s from %s, sending to %s' % (req, self.client_address[0], getServerAddr(servID)))
+        serv_sock = getServerSocket(servID)
+        serv_sock.sendall(req)
+        data = serv_sock.recv(2)
+        client_sock.sendall(data)
+        client_sock.close()
+
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
+
 
 if __name__ == '__main__':
     try:
         LBPrint('LB Started')
         LBPrint('Connecting to servers')
-        for name, (addr, sock) in servers.items():
-            servers[name] = (addr, createSocket(addr, HTTP_PORT))
+        for name, (addr, sock) in servers.iteritems():
+            servers[name] = (
+             addr, createSocket(addr, HTTP_PORT))
 
         server = ThreadedTCPServer((SERV_HOST, HTTP_PORT), LoadBalancerRequestHandler)
-        
-        # Start a thread to process requests from the queue
-        queue_processor_thread = threading.Thread(target=processQueue)
-        queue_processor_thread.daemon = True
-        queue_processor_thread.start()
-        
         server.serve_forever()
     except socket.error as msg:
         LBPrint(msg)
+
+# okay decompiling exampleLB.pyc
