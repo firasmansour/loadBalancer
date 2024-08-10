7e092dbc008b520dc57db05700e0fbe0f1e23b23
firasmansour <103880221+firasmansour@users.noreply.github.com>
2024-08-11 00:07:59 +0300

    Update loadBalancer.py

--- loadBalancer.py
+++ loadBalancer.py
@@ -65,7 +65,7 @@ def getNextServer(req_type, req_time):
             next_server = 2
         elif previous_server == 2 :
             next_server = 1
-    else 
+    else:
         next_server = 3
     previous_server = next_server
     lock.release()
